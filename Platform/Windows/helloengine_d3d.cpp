4c35fa50a57940a4cfd924d44469613131111db3
王彬彬 <wbb18@mails.tsinghua.edu.cn>
2020-01-13 15:08:01 +0800

    ..

--- Platform/Windows/helloengine_d3d.cpp
+++ Platform/Windows/helloengine_d3d.cpp
@@ -1,9 +1,9 @@
-// include the basic windows header file
 #include <windows.h>
 #include <windowsx.h>
 #include <tchar.h>
-// #include <d2d1.h>
 #include <stdint.h>
+
+// #include <d2d1.h>
 #include <d3d11.h>
 #include <d3d11_1.h>
 #include <d3dcompiler.h>
@@ -11,258 +11,99 @@
 #include <DirectXPackedVector.h>
 #include <DirectXColors.h>
 
-ID2D1Factory                 *pFactory = nullptr;
-ID2D1HwndRenderTarget		 *pRenderTarget = nullptr;
-ID2D1SolidColorBrush         *pLightSlateGrayBrush = nullptr;
-ID2D1SolidColorBrush         *pCornflowerBlueBrush = nullptr;
+// ID2D1Factory                 *pFactory = nullptr;
+// ID2D1HwndRenderTarget		 *pRenderTarget = nullptr;
+// ID2D1SolidColorBrush         *pLightSlateGrayBrush = nullptr;
+// ID2D1SolidColorBrush         *pCornflowerBlueBrush = nullptr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+const uint32_t SCREEN_WIDTH  = 960;
+const uint32_t SCREEN_HEIGHT = 480;
+
+// global declarations
+IDXGISwapChain			*g_pSwapchain	= nullptr;	// the pointer to the swap chain interface 
+ID3D11Device			*g_pDev			= nullptr;	// the pointer to our Direct3D device interface 
+ID3D11DeviceContext		*g_pDevcon		= nullptr;	// the pointer to our Direct3D device context 
+
+ID3D11RenderTargetView	*g_pRTView		= nullptr;
+
+ID3D11InputLayout		*g_pLayout		= nullptr;	// the pointer to the input layour
+ID3D11VertexShader		*g_pVS			= nullptr;	// the pointer to the vertex shader 
+ID3D11PixelShader		*g_pPS			= nullptr;	// the pointer to the pixel shader 
+
+ID3D11Buffer			*g_pVBuffer		= nullptr;	// Vertex Buffer 
+
+// vertex buffer structure
+struct VERTEX {
+	XMLFLOAT3	Position;
+	XMLFLOAT4	Color;
+};
 
 template<class T>
-inline void SafeRelease(T **ppInterfaceToRelease)
-{
-	if (*ppInterfaceToRelease != nullptr)
-	{
+inline void SafeRelease(T **ppInterfaceToRelease) {
+	if (*ppInterfaceToRelease != nullptr) {
 		(*ppInterfaceToRelease)->Release();
 		(*ppInterfaceToRelease) = nullptr;
 	}
 }
 
-HRESULT CreateGraphicsResources(HWND hWnd)
-{
-	HRESULT hr = S_OK;
-	if (pRenderTarget == nullptr)
-	{
-		RECT rc;
-		GetClientRect(hWnd, &rc);
+void CreateRenderTarget() {
+	HRESULT hr;
+	ID3D11Texture2D *pBackBuffer;
 
-		D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+	// Get a pointer to the back buffer
+	g_pSwapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
 
-		hr = pFactory->CreateHwndRenderTarget(
-			D2D1::RenderTargetProperties(),
-			D2D1::HwndRenderTargetProperties(hWnd, size),
-			&pRenderTarget);
+	// Create a render-target view 
+	g_pDev->CreateRenderTargetView(pBackBuffer, NULL, &g_pRTView); 
 
-		if (SUCCEEDED(hr))
-		{
-			hr = pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::LightSlateGray), &pLightSlateGrayBrush);
-		}
+	pBackBuffer->Release(); 
 
-		if (SUCCEEDED(hr))
-		{
-			hr = pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::CornflowerBlue), &pCornflowerBlueBrush);
-		}
-	}
-	return hr;
+	// Bind the view 
+	g_pDevcon->OMSetRenderTargets(1, &g_pRTView, NULL);
 }
 
-void DiscardGraphicsResources()
-{
-	SafeRelease(&pRenderTarget);
-	SafeRelease(&pLightSlateGrayBrush);
-	SafeRelease(&pCornflowerBlueBrush);
+void SetViewPort() {
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = SCREEN_WIDTH;
+	viewport.Height = SCREEN_HEIGHT;
+
+	g_pDevcon->RSSetViewports(1, &viewport);
 }
 
-// the WindowProc function prototype
-LRESULT CALLBACK WindowProc(HWND hWnd, 
-							UINT message,
-							WPARAM wParam,
-							LPARAM lParam);
+// this is the function that loads and prepares the shaders
+void InitPipline() {
+	// load and compile the two shaders
+	ID3DBlob *VS, *PS;
 
-// the entry point for any Windows program
- int WINAPI WinMain(HINSTANCE hInstance,
-					HINSTANCE hPrevInstance,
-					LPTSTR lpCmdLine,
-					int nCmdShow)
- {
-	 // the handle for the window, filled by a function
-	 HWND hWnd;
-	 // this struct holds information for the windows class
-	 WNDCLASSEX wc;
+	D3DReadFileToBlob(L"copy.vso", &VS);
+	D3DReadFileToBlob(L"copy.pso", &PS);
 
-	 // initialize COM
-	 if (FAILED(CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE)))
-		 return -1;
-	
-	 // clear out the window class for use
-	 ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	// encapsulate both shaders into shader objects
+	g_pDev->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &g_pVS);
+	g_pDev->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &g_pPS);
 
-	 // fill in the struct with the needed information
-	 wc.cbSize = sizeof(WNDCLASSEX);
-	 wc.style = CS_HREDRAW | CS_VREDRAW;
-	 wc.lpfnWndProc = WindowProc;
-	 wc.hInstance = hInstance;
-	 // wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	 wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	 wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	 wc.lpszClassName = _T("WindowClass1");
+	// set the shader objects
+	g_pDevcon->VSSetShader(g_pVS, 0, 0);
+	g_pDevcon->PSSetShader(g_pPS, 0, 0);
 
-	 // register the window class
-	 RegisterClassEx(&wc);
+	// create the input layout object 
+	D3D11_INPUT_ELEMENT_DESC ied[] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	}; 
 
-	 // create the window and use the result as the handle
-	 hWnd = CreateWindowEx(0,
-						   _T("WindowClass1"),		// name of the window
-						   _T("Hello, Engine![Direct 2D]"),	// title of the window
-						   WS_OVERLAPPEDWINDOW,		// window style
-						   100,
-						   100,
-						   960,
-						   540,
-						   NULL,	// we have no parent window, NULL
-						   NULL,	// we aren't using menus, NULL
-						   hInstance,	// application handle
-						   NULL);	// used with multiple windows, NULL
-	// display the window on the screen
-	ShowWindow(hWnd, nCmdShow);
-	
-	// enter the main loop:
+	g_pDev->CreateInputLayout(ied, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &g_pLayout);
+	g_pDevcon->IASetInputLayout(g_pLayout);
 
-	// this struct holds Windows event messages
-	MSG msg;
-
-	// wait for the next message in the queue, store the resutl in 'msg'
-	//while (GetMessage(&msg, NULL, 0, 0))
-	while (GetMessage(&msg, nullptr, 0, 0))
-	{
-		// translate keystroke messages into the right format
-		TranslateMessage(&msg);
-
-		// send the message to the WindowProc function
-		DispatchMessage(&msg);
-	}
-
-	// uninitialize COM
-	CoUninitialize();
-
-	// return this part of the WM_QUIT message to Windows
-	return msg.wParam;
+	VS->Release();
+	PS->Release();
 }
 
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	LRESULT result = 0;
-	bool wasHandled = false;
-
-	// sort through and find what code to run for the message given 
-	switch(message)
-	{
-		case WM_CREATE:
-			if (FAILED(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory)))
-			{
-				result = -1;	// Fail CreateWindowEx.
-				return result;
-			}
-			wasHandled = true;
-			result = 0;
-			break;
-
-		case WM_PAINT:
-			{
-				HRESULT hr = CreateGraphicsResources(hWnd);
-				if (SUCCEEDED(hr))
-				{
-					PAINTSTRUCT ps;
-					BeginPaint(hWnd, &ps);
-
-					// start build GPU draw command
-					pRenderTarget->BeginDraw();
-
-					// clear the background with white color
-					pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
-
-					// retrieve the size of drawing area
-					D2D1_SIZE_F rtSize = pRenderTarget->GetSize();
-
-					// draw a grid background.
-					int width = static_cast<int>(rtSize.width);
-					int height = static_cast<int>(rtSize.height);
-
-					for (int x = 0; x < width; x += 10)
-					{
-						pRenderTarget->DrawLine(
-							D2D1::Point2F(static_cast<FLOAT>(x), 0.0f),
-							D2D1::Point2F(static_cast<FLOAT>(x), rtSize.height),
-							pLightSlateGrayBrush,
-							0.5f
-							);
-					}
-
-					for (int y = 0; y < height; y += 10)
-					{
-						pRenderTarget->DrawLine(
-							D2D1::Point2F(0.0f, static_cast<FLOAT>(y)),
-							D2D1::Point2F(rtSize.width, static_cast<FLOAT>(y)),
-							pLightSlateGrayBrush,
-							0.5f
-							);
-					}
-
-					// draw two rectangeles
-					D2D1_RECT_F rectangle1 = D2D1::RectF(
-						rtSize.width / 2 - 50.0f,
-						rtSize.height / 2 - 50.0f,
-						rtSize.width / 2 + 50.0f,
-						rtSize.height / 2 + 50.0f
-						);
-					
-					D2D1_RECT_F rectangle2 = D2D1::RectF(
-						rtSize.width / 2 - 100.0f,
-						rtSize.height / 2 - 100.0f,
-						rtSize.width / 2 + 100.0f,
-						rtSize.height / 2 + 100.0f
-						);
-
-					// draw a filled rectangle
-					pRenderTarget->FillRectangle(&rectangle1, pLightSlateGrayBrush);
-
-					// draw a outline only rectangle
-					pRenderTarget->FillRectangle(&rectangle2, pLightSlateGrayBrush);
-
-					// end GPU draw command building
-					hr = pRenderTarget->EndDraw();
-					if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
-					{
-						DiscardGraphicsResources();
-					}
-
-					EndPaint(hWnd, &ps);
-				}
-			}
-			wasHandled = true;
-			break;
-		
-		case WM_SIZE:
-			if (pRenderTarget != nullptr)
-			{
-				RECT rc;
-				GetClientRect(hWnd, &rc);
-				D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
-				pRenderTarget->Resize(size);
-			}
-			wasHandled = true;
-			break;
-
-		case WM_DESTROY:
-			DiscardGraphicsResources();
-			if (pFactory)
-			{
-				pFactory->Release();
-				pFactory = nullptr;
-			}
-			PostQuitMessage(0);
-			result = 0;
-			wasHandled = true;
-			break;
-
-		case WM_DISPLAYCHANGE:
-			InvalidateRect(hWnd, nullptr, false);
-			wasHandled = true;
-			break;
-	}
-	
-	if (!wasHandled) 
-	{
-		result = DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return result;
-}
+// this is the function that creates the shape to render 
 
--- README.md
+++ README.md
@@ -22,3 +22,27 @@
 
 ```>cl user32.lib ole32.lib d2d1.lib helloengine_d2d.cpp```
 
+
+
+
+
+1.  在环境变量中Path 那一项中添加两个路径（记得加分号）：
+    `E:\VS2017\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64;`
+    `E:\VS2017\Common7\IDE`
+    第一行是编译器的目录，根据电脑选吧，但是至少HostX86\x86是所有平台都能用，64位系统就像例子中那么填
+2.  在环境变量中新建一个LIB 变量，并添加三个路径（记得加分号）：
+    `C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.25017\lib\x64;`
+    `C:\Program Files (x86)\Windows Kits\10\Lib\10.0.14393.0\ucrt\x86;`
+    `C:\Program Files (x86)\Windows Kits\8.1\Lib\winv6.3\um\x86;`
+    第一个路径填的是lib路径，如果在path变量中填了HostX64\x64的话就用x64的lib
+    第二个路径填的我也不知道，但是填就填最新版的话，目前没发现什么问题
+3.  在环境变量中新建一个INCLUDE 变量，并添加两个路径（记得加分号）：
+    `E:\VS2017\VC\Tools\MSVC\14.10.25017\include;`
+    `C:\Program Files (x86)\Windows Kits\10\Include\10.0.14393.0\ucrt;`
+    第二个路径和上面第二个路径对应
+4.  在E:\VS2017\VC\Auxiliary\Build 这个路径中点击vcvarall.bat 这个批处理程序。之后打开命令行，输入cl，如果没有报错，则配置成功……
+
+*安装的时候要选定SDK 10*
+*实际路径根据自己的安装路径有所不同。*
+
+>   参考：http://zuciwang.com/show/229362.html
